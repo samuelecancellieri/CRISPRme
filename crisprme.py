c5eb7ec31a91064bef349a7419e5fb7fc596153c
Samuele_Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2020-10-27 19:28:14 +0100

    ddd

---
+++ crisprme.py
@@ -0,0 +1,282 @@
+#!/usr/bin/env python
+# Python Program to search CRISPR/Cas complex into a genome
+import os
+import sys
+import subprocess					# run c++ executable
+import time
+import os							# instructions manage directories
+from os import listdir
+from os.path import isfile, join, isdir
+import shutil						# remove directory and its content
+import sys							# input argv
+from subprocess import Popen, PIPE
+import glob
+
+# path where this file is located
+origin_path = os.path.dirname(os.path.realpath(__file__))
+# conda path
+conda_path = "opt/crisprme/"
+# path corrected to use with conda
+corrected_origin_path = origin_path[:-3]+conda_path
+
+input_args = sys.argv
+
+variant = True
+
+
+def complete_search():
+    if "--help" in input_args:
+        print("This is the automated search process that goes from raw input up to the post-analysis of results.")
+        print("These are the flags that must be used in order to run this function:")
+        print("\t--genome , used to specify the reference genome folder")
+        print("\t--vcf , used to specify the VCF folder [OPTIONAL!]")
+        print(
+            "\t--guide , used to specify the file that contains guides used for the search")
+        print("\t--pam , used to specify the file that contains the pam")
+        print("\t--annotation , used to specify the file that contains some annotations of the reference genome")
+        print(
+            "\t--samplesID , used to specify the file that contains the information about samples present in VCF files [OPTIONAL!]")
+        print("\t--bMax , used to specify the number of bulges for the indexing of the genome(s)")
+        print("\t--mm , used to specify the number of mismatches permitted in the search phase")
+        print("\t--bDNA , used to specify the number of DNA bulges permitted in the search phase")
+        print("\t--bRNA , used to specify the number of RNA bulges permitted in the search phase")
+        print("\t--output , used to specify the output folder for the results")
+        exit(0)
+
+    if "--genome" not in input_args:
+        print("--genome must be contained in the input")
+        exit(1)
+    else:
+        try:
+            genomedir = input_args[input_args.index("--genome")+1]
+        except IndexError:
+            print("Please input some parameter for flag --genome")
+            exit(1)
+        if not os.path.isdir(genomedir):
+            print("The folder specified for --genome does not exist")
+            exit(1)
+
+    if "--vcf" not in input_args:
+        variant = False
+    else:
+        try:
+            vcfdir = input_args[input_args.index("--vcf")+1]
+        except IndexError:
+            print("Please input some parameter for flag --vcf")
+            exit(1)
+        if not os.path.isdir(vcfdir):
+            print("The folder specified for --vcf does not exist")
+            exit(1)
+
+    if "--guide" not in input_args:
+        print("--guide must be contained in the input")
+        exit(1)
+    else:
+        try:
+            guidefile = input_args[input_args.index("--guide")+1]
+        except IndexError:
+            print("Please input some parameter for flag --guide")
+            exit(1)
+        if not os.path.isfile(guidefile):
+            print("The folder specified for --guide does not exist")
+            exit(1)
+
+    if "--pam" not in input_args:
+        print("--pam must be contained in the input")
+        exit(1)
+    else:
+        try:
+            pamfile = input_args[input_args.index("--pam")+1]
+        except IndexError:
+            print("Please input some parameter for flag --pam")
+            exit(1)
+        if not os.path.isfile(pamfile):
+            print("The folder specified for --pam does not exist")
+            exit(1)
+
+    if "--annotation" not in input_args:
+        print("--annotation must be contained in the input")
+        exit(1)
+    else:
+        try:
+            annotationfile = input_args[input_args.index("--annotation")+1]
+        except IndexError:
+            print("Please input some parameter for flag --annotation")
+            exit(1)
+        if not os.path.isfile(annotationfile):
+            print("The folder specified for --annotation does not exist")
+            exit(1)
+
+    if variant and "--samplesID" not in input_args:
+        print("--samplesID must be contained in the input")
+        exit(1)
+    elif not variant and "--samplesID" in input_args:
+        print("--samplesID was in the input but no VCF directory was specified")
+        exit(1)
+    elif "--samplesID" in input_args:
+        try:
+            samplefile = input_args[input_args.index("--samplesID")+1]
+        except IndexError:
+            print("Please input some parameter for flag --samplesID")
+            exit(1)
+        if not os.path.isfile(samplefile):
+            print("The folder specified for --samplesID does not exist")
+            exit(1)
+
+    if "--bMax" not in input_args:
+        print("--bMax must be contained in the input")
+        exit(1)
+    else:
+        try:
+            bMax = input_args[input_args.index("--bMax")+1]
+        except IndexError:
+            print("Please input some parameter for flag --bMax")
+            exit(1)
+        try:
+            bMax = int(bMax)
+        except:
+            print("Please input a number for flag bMax")
+            exit(1)
+        if bMax < 0 or bMax > 2:
+            print("The range for bMax is from 0 to 2")
+            exit(1)
+
+    if "--mm" not in input_args:
+        print("--mm must be contained in the input")
+        exit(1)
+    else:
+        try:
+            mm = input_args[input_args.index("--mm")+1]
+        except IndexError:
+            print("Please input some parameter for flag --mm")
+            exit(1)
+        try:
+            mm = int(mm)
+        except:
+            print("Please input a number for flag mm")
+            exit(1)
+
+    if "--bDNA" not in input_args:
+        print("--bDNA must be contained in the input")
+        exit(1)
+    else:
+        try:
+            bDNA = input_args[input_args.index("--bDNA")+1]
+        except IndexError:
+            print("Please input some parameter for flag --bDNA")
+            exit(1)
+        try:
+            bDNA = int(bDNA)
+        except:
+            print("Please input a number for flag bDNA")
+            exit(1)
+        if bDNA > bMax:
+            print("The number of bDNA must be equal or less than bMax")
+            exit(1)
+        elif bDNA < 0 or bDNA > 2:
+            print("The range for bDNA is from 0 to", bMax)
+            exit(1)
+
+    if "--bRNA" not in input_args:
+        print("--bRNA must be contained in the input")
+        exit(1)
+    else:
+        try:
+            bRNA = input_args[input_args.index("--bRNA")+1]
+        except IndexError:
+            print("Please input some parameter for flag --bRNA")
+            exit(1)
+        try:
+            bRNA = int(bRNA)
+        except:
+            print("Please input a number for flag bRNA")
+            exit(1)
+        if bRNA > bMax:
+            print("The number of bRNA must be equal or less than bMax")
+            exit(1)
+        elif bRNA < 0 or bRNA > 2:
+            print("The range for bRNA is from 0 to", bMax)
+            exit(1)
+
+    if "--output" not in input_args:
+        print("--output must be contained in the input")
+        exit(1)
+    else:
+        try:
+            outputfolder = input_args[input_args.index("--output")+1]
+        except IndexError:
+            print("Please input some parameter for flag --output")
+            exit(1)
+        if not os.path.isdir(outputfolder):
+            print("The folder specified for --output does not exist")
+            exit(1)
+
+    if variant:
+        os.system(corrected_origin_path+"./automated_search.sh "+genomedir+" "+vcfdir+" "+guidefile+" "+pamfile+" " +
+                  annotationfile+" "+samplefile+" "+str(bMax)+" "+str(mm)+" "+str(bDNA)+" "+str(bRNA)+" "+outputfolder)
+    else:
+        os.system(corrected_origin_path+"./automated_search.sh "+genomedir+" _ "+guidefile+" "+pamfile+" " +
+                  annotationfile+" _ "+str(bMax)+" "+str(mm)+" "+str(bDNA)+" "+str(bRNA)+" "+outputfolder)
+
+
+# HELP FUNCTION
+def callHelp():
+    print("help:\n",
+          "\nALL FASTA FILEs USED BY THE SOFTWARE MUST BE UNZIPPED AND CHROMOSOME SEPARATED, ALL VCFs USED BY THE SOFTWARE MUST BE ZIPPED AND CHROMOSOME SEPARATED",
+          # "\ncrispritz graphical-interface FUNCTION TO START LOCAL SERVER AND ACTIVATE USER GRAPHICAL INTERFACE"
+          "\ncrispritz version FUNCTION RETURNING THE VERSION OF CRISPRitz INSTALLED",
+          "\ncrispritz add-variants FUNCTION TO ADD VARIANTS DATA TO A FASTA GENOME",
+          "\ncrispritz index-genome FUNCTION TO CREATE GENOME INDEX TO PERFORM FAST SEARCHES WITH BULGES",
+          "\ncrispritz search FUNCTION TO PERFORM SEARCHES ON A GENOME (INDEXED OR PLAIN FASTA)",
+          "\ncrispritz scores FUNCTION TO CALCULATE THE CFD SCORE FOR A LIST OF TARGETS",
+          "\ncrispritz annotate-results FUNCTION TO ADD GENOMIC INFORMATION TO TARGETS RESULTS",
+          "\ncrispritz generate-report FUNCTION TO GENERATE GRAPHICAL REPORT FOR A SPECIFIC GUIDE",
+          "\ncrispritz process-data FUNCTION TO ANALYZE RESULTS TO GENERATE VARIANT ANALYSIS AND SAMPLE CLASSIFICATION (beta)",
+          "\n\nADD help TO ANY FUNCTION TO VISUALIZE A BRIEF HELP PAGE (example: crispritz index-genome help)\n")
+
+
+def graphicalInterface():
+    # start graphical interface
+    if ('help' in sys.argv[1:]):  # TODO completare
+        print('Open the Graphical Interface for CRISPRitz.',
+              "\nEXAMPLE CALL: crispritz.py graphical-interface <serverDirectory>\n",
+              '\n<serverDirectory>: (Optional) directory where to start the server for the graphical interface. If not provided, the current directory will be used\n',
+              'Please note that the <serverDirectory> must be structured as such:\n',
+              '-annotations\n\t-<annotationsFile>: One or more text files containing the annotations in .bed format\n',
+              '-assets\n',
+              '-dictionaries\n\t-dictionary_name\n',
+              '-genome_library\n',
+              '-Genomes\n',
+              '-pam\n',
+              '-Results')
+        sys.exit()
+    try:
+        launch_dir = os.path.realpath(sys.argv[2])
+    except:
+        launch_dir = os.path.realpath('./')
+    if not os.path.isdir(launch_dir):
+        print('Directory not found. Creating...')
+        os.mkdir(launch_dir)
+    os.chdir(launch_dir)
+    check_directories = ['annotations', 'assets', 'dictionaries',
+                         'genome_library', 'Genomes', 'pam', 'Results']
+    for d in check_directories:
+        if not os.path.isdir(os.path.realpath(d)):
+            print(d + ' directory not found. Creating...')
+            os.mkdir(os.getcwd() + '/' + d)
+
+    print("LAUNCHING GRAPHICAL SERVER...")
+    subprocess.run(
+        [corrected_origin_path + 'Python_Scripts/CrispritzWebApp/app_v6.py'])
+    time.sleep(1)
+    print("COPY THE FOLLOWING LINK INTO ANY BROWSER: http//:localhost")
+
+
+if len(sys.argv) < 2:
+    callHelp()
+elif sys.argv[1] == 'graphical-interface':
+    graphicalInterface()
+elif sys.argv[1] == 'complete-search':
+    complete_search()
+else:
+    print("ERROR! \"" + sys.argv[1] + "\" is not an allowed!")
