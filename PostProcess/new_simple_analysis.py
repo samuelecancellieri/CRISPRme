f19c740673dff1798789fb596001614c8ccf79c2
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-03-01 23:30:30 +0100

    aa

--- PostProcess/new_simple_analysis.py
+++ PostProcess/new_simple_analysis.py
@@ -62,52 +62,52 @@ listCluster = []
 for line in inTarget:
     split = line.strip().split('\t')
     guide_no_bulge = split[1].replace('-', '')
-    if (guide_no_bulge + split[3] + split[5] + split[6]) == current_guide_chr_pos_direction:
-        realTarget = split[2]
-        replaceTarget = split[2].replace('-', '')
-        refSeq = genomeStr[split[4]:split[4]+len(guide_no_bulge)]
-        replaceTargetsDict = dict()
+    # if (guide_no_bulge + split[3] + split[5] + split[6]) == current_guide_chr_pos_direction:
+    realTarget = split[2]
+    replaceTarget = split[2].replace('-', '')
+    refSeq = genomeStr[split[4]:split[4]+len(guide_no_bulge)]
+    replaceTargetsDict = dict()
 
-        totalDict = dict()
-        for pos_c, c in enumerate(replaceTarget):
-            if c in iupac_code:
-                snpToReplace, sampleSet, rsID, AF_var, snpInfo = retrieveFromDict(
-                    pos_c+int(split[4]))
-                for i, elem in enumerate(snpToReplace):
-                    listReplaceTarget = list(refSeq)
-                    listReplaceTarget[pos_c] = elem
-                    totalDict[(pos_c, elem)] = dict()
-                    listInfo = [[rsID[i], AF_var[i], snpInfo[i]]]
-                    totalDict[pos_c][0] = [listReplaceTarget,
-                                           sampleSet[i], listInfo]
+    totalDict = dict()
+    for pos_c, c in enumerate(replaceTarget):
+        if c in iupac_code:
+            snpToReplace, sampleSet, rsID, AF_var, snpInfo = retrieveFromDict(
+                pos_c+int(split[4]))
+            for i, elem in enumerate(snpToReplace):
+                listReplaceTarget = list(refSeq)
+                listReplaceTarget[pos_c] = elem
+                totalDict[(pos_c, elem)] = dict()
+                listInfo = [[rsID[i], AF_var[i], snpInfo[i]]]
+                totalDict[pos_c][0] = [listReplaceTarget,
+                                       sampleSet[i], listInfo]
 
-        # the time of the universe
-        for key in totalDict:  # for each snp in target (fixpoint)
-            count = 0  # to create levels of tuple (couples,triplets,...)
-            # for each other snp in target (> fixpoint)
-            for newkey in totalDict:
-                if newkey[1] > key[1]:
-                    resultSet = totalDict[key][count][1].intersection(
-                        totalDict[newkey][0][1])  # extract intersection of sample to generate possible multisnp target
-                    if len(resultSet) > 0:  # if set is not null
-                        # go forward one level (couples,triplets,...)
-                        count += 1
-                        # add new snp to preceding target seq with snp
-                        replaceTarget1 = totalDict[newkey][0][0]
-                        replaceTarget2 = totalDict[key][count-1][0]
-                        replaceTarget2[newkey] = replaceTarget1[newkey]
-                        listInfo2 = totalDict[key][count-1][2].copy()
-                        listInfo2.append(totalDict[newkey][0][2])
-                        # add to new level the modified seq and set of samples and info of snp
-                        totalDict[key][count] = [
-                            replaceTarget2, resultSet, listInfo2]
-                        # remove the new generated sample set from all lower levels
-                        totalDict[key][count-1][1] = totalDict[key][count - 1][1] - \
-                            totalDict[key][count][1]
-                        totalDict[newkey][0][1] = totalDict[newkey][0][1] - \
-                            totalDict[key][count][1]
+    # the time of the universe
+    for key in totalDict:  # for each snp in target (fixpoint)
+        count = 0  # to create levels of tuple (couples,triplets,...)
+        # for each other snp in target (> fixpoint)
+        for newkey in totalDict:
+            if newkey[1] > key[1]:
+                resultSet = totalDict[key][count][1].intersection(
+                    totalDict[newkey][0][1])  # extract intersection of sample to generate possible multisnp target
+                if len(resultSet) > 0:  # if set is not null
+                    # go forward one level (couples,triplets,...)
+                    count += 1
+                    # add new snp to preceding target seq with snp
+                    replaceTarget1 = totalDict[newkey][0][0]
+                    replaceTarget2 = totalDict[key][count-1][0]
+                    replaceTarget2[newkey] = replaceTarget1[newkey]
+                    listInfo2 = totalDict[key][count-1][2].copy()
+                    listInfo2.append(totalDict[newkey][0][2])
+                    # add to new level the modified seq and set of samples and info of snp
+                    totalDict[key][count] = [
+                        replaceTarget2, resultSet, listInfo2]
+                    # remove the new generated sample set from all lower levels
+                    totalDict[key][count-1][1] = totalDict[key][count - 1][1] - \
+                        totalDict[key][count][1]
+                    totalDict[newkey][0][1] = totalDict[newkey][0][1] - \
+                        totalDict[key][count][1]
 
-        for key in totalDict:
-            for level in totalDict[key]:
-                if len(totalDict[key][level][1]) > 0:
-                    print(totalDict[key][level])
+    for key in totalDict:
+        for level in totalDict[key]:
+            if len(totalDict[key][level][1]) > 0:
+                print(totalDict[key][level])
