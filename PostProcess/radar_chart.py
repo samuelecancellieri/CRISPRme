ae071323bc9e8df4172bfeae078a6306be832474
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-03-09 01:30:58 +0100

    test

--- PostProcess/radar_chart.py
+++ PostProcess/radar_chart.py
@@ -157,11 +157,11 @@ def generatePlot(guide, guideDict, motifDict, mismatch, bulge, source):
     p4 = plt.bar(ind, T, width, bottom=C+G+A, align='center')
     p5 = plt.bar(ind, RNA, width, bottom=C+G+A+T, align='center')
     p6 = plt.bar(ind, DNA, width, bottom=C+G+A+T+RNA,
-                 align='center', label=list(guide))
+                 align='center')
 
     plt.xlim(0, len(guide))
     # strArray = np.array([list(guide)])
-    # plt.xticks(ticks=ind, labels=list(guide))
+    plt.xticks(ticks=ind, labels=list(guide))
 
     plt.legend((p1[0], p2[0], p3[0], p4[0], p5[0], p6[0]),
                ('A', 'C', 'G', 'T', 'RNA', 'DNA'), fontsize=13, loc='upper right', ncol=6)
