79937a542a85263f42b1d669a8c8b73d5e412fdd
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-03-26 15:33:21 +0100

    Update crisprme_auto_test.sh

--- crisprme_auto_test.sh
+++ crisprme_auto_test.sh
@@ -1,7 +1,7 @@
 #crisprme download and test
 
 #first download all the necessary data and test folders
-
+wget https://github.com/samuelecancellieri/CRISPRme/raw/main/crisprme_folders.tar.gz
 
 
 ftp://ngs.sanger.ac.uk:21/production/hgdp/hgdp_wgs.20190516/hgdp_wgs.20190516.full.chr10.vcf.gz">hgdp_wgs.20190516.full.chr10.vcf.gz
