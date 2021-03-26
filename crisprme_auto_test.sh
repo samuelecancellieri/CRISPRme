8c48c1404d9e12fa12a118981c141401f2d3dbd1
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-03-26 15:17:21 +0100

    Add files via upload

---
+++ crisprme_auto_test.sh
@@ -0,0 +1,7 @@
+#crisprme download and test
+
+#first download all the necessary data and test folders
+
+
+
+ftp://ngs.sanger.ac.uk:21/production/hgdp/hgdp_wgs.20190516/hgdp_wgs.20190516.full.chr10.vcf.gz">hgdp_wgs.20190516.full.chr10.vcf.gz
