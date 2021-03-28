5fdcff0e19ea42ef3fd4067bd01d245780d86e0f
fmasillo <75281014+fmasillo@users.noreply.github.com>
2021-03-28 19:10:21 +0200

    Revert "Update new_simple_analysis.py"
    This reverts commit 215b6ce7f9783f1ec4afe2780b7b6b937c60b82e.

--- PostProcess/new_simple_analysis.py
+++ PostProcess/new_simple_analysis.py
@@ -334,12 +334,8 @@ for line in inTarget:
         if c in iupac_code:
             # print(c)
             countIUPAC += 1
-            if split[0] == "RNA" and split[6] == '+':
-                snpToReplace, sampleSet, rsID, AF_var, snpInfo = retrieveFromDict(
-                    pos_c+int(split[5]))
-            else:
-                snpToReplace, sampleSet, rsID, AF_var, snpInfo = retrieveFromDict(
-                    pos_c+int(split[4]))
+            snpToReplace, sampleSet, rsID, AF_var, snpInfo = retrieveFromDict(
+                pos_c+int(split[4]))
             for i, elem in enumerate(snpToReplace):
                 listReplaceTarget = list(refSeq)
                 listReplaceTarget[pos_c] = elem
