3bbd7b3d748485212303f8e0e1e0e72925d834bf
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-04-03 10:36:01 +0200

    test split

--- pages/main_page.py
+++ pages/main_page.py
@@ -326,7 +326,7 @@ def changeUrl(n, href, nuclease, genome_selected, ref_var, annotation_var, vcf_i
         # Extract sequence and create the guides
         guides = []
         extracted_seqs = list()
-        for lines in text_guides.split('\n'):
+        for lines in text_guides:
             print('linea', lines)
         for name_and_seq in text_sequence.split('>'):
             if '' == name_and_seq:
