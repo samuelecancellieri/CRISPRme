2a660f2b535c599f3bce231717df0695e227cec1
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-04-13 17:20:01 +0200

    test image creation

--- pages/results_page.py
+++ pages/results_page.py
@@ -2494,8 +2494,7 @@ def generate_sample_card(n, sample, sel_cel, all_guides, search):
         # plot for images in personal card
         os.system(f"python {app_main_directory}/PostProcess/CRISPRme_plots.py {integrated_personal} {current_working_directory}/Results/{job_id}/imgs/ {guide}.personal &> {current_working_directory}/Results/{job_id}/warnings.txt")
         os.system(f"python {app_main_directory}/PostProcess/CRISPRme_plots.py {integrated_private} {current_working_directory}/Results/{job_id}/imgs/ {guide}.private &> {current_working_directory}/Results/{job_id}/warnings.txt")
-        os.system(
-            f"rm -f {current_working_directory}/Results/{job_id}/warnings.txt {integrated_private} {integrated_personal} &")
+        # os.system(f"rm -f {current_working_directory}/Results/{job_id}/warnings.txt {integrated_private} {integrated_personal}")
 
         private = 0
         for line in open(sample_grep_result):
@@ -2553,9 +2552,13 @@ def generate_sample_card(n, sample, sel_cel, all_guides, search):
                 ans.columns = c.split('\t')[:23]
 
     # image for personal and private
-    # image_personal_top = 'data:image/png;base64,{}'.format(base64.b64encode(open(current_working_directory + 'Results/' + job_id + f'/imgs/CRISPRme_top_1000_log_for_main_text_{guide}.personal.png', 'rb').read()).decode())
-    # image_private_top = 'data:image/png;base64,{}'.format(base64.b64encode(open(current_working_directory + 'Results/' + job_id + f'/imgss/CRISPRme_top_1000_log_for_main_text_{guide}.private.png', 'rb').read()).decode())
+    image_personal_top = 'data:image/png;base64,{}'.format(base64.b64encode(open(
+        current_working_directory + 'Results/' + job_id + f'/imgs/CRISPRme_top_1000_log_for_main_text_{guide}.personal.png', 'rb').read()).decode())
+    image_private_top = 'data:image/png;base64,{}'.format(base64.b64encode(open(
+        current_working_directory + 'Results/' + job_id + f'/imgss/CRISPRme_top_1000_log_for_main_text_{guide}.private.png', 'rb').read()).decode())
 
+    os.system(
+        f"rm -f {current_working_directory}/Results/{job_id}/warnings.txt {integrated_private} {integrated_personal}")
     try:
         file_to_load = job_id + '.' + sample + '.tmp_card.zip'
         ans[''] = [''] * ans.shape[0]  # taaaaaaaaaac
