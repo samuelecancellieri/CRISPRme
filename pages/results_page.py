d52841be734f3012a1f11b0bdeca0a6bc2b93362
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-04-16 19:35:45 +0200

    test graph summ

--- pages/results_page.py
+++ pages/results_page.py
@@ -3324,7 +3324,7 @@ def updateContentTab(value, sel_cel, all_guides, search, genome_type):
                                      style={'width': '60px'}
                                      )
                     ]
-                )
+                ), md=4
             ),
             dbc.Col(
                 html.Div(
@@ -3337,7 +3337,7 @@ def updateContentTab(value, sel_cel, all_guides, search, genome_type):
                                      style={'width': '60px'}
                                      )
                     ]
-                )
+                ), md=4
             )
         ]
         # DA SPOSTARE DOPO BARPLOT E RADCHART TOTAL
@@ -3389,11 +3389,9 @@ def updateContentTab(value, sel_cel, all_guides, search, genome_type):
                         dbc.Col(top1000_image, width={"size": 10, "offset": 2})
                     ),
                     dbc.Row(
-                        [
-                            dbc.Col(
-                                html.Div(total_buttons),
-                            )
-                        ], align="center",
+                        dbc.Col(
+                            html.Div(total_buttons),
+                        )
                     )
                 ]
             )
