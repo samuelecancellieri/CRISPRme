1a3f93ed0fc9c6f66c484a20b362c564faa5df14
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-04-20 18:10:45 +0200

    Website (#4)
    * inserted gnomad converter into main wrapper
    
    * fix wrapper
    
    * test convert gnomAd update names
    
    * update help page
    
    * update help page figures
    
    * remove all images from help and result page
    
    * Re update images
    
    * Update results_page.py
    
    * test download summary by sample table
    
    * ss
    
    * test file to load
    
    * download sample tab
    
    * update to download sample
    
    * permission
    
    * Revert "permission"
    
    This reverts commit d067f8e5660f4743759f10296be07415e8079f5d.
    
    Co-authored-by: fmasillo <75281014+fmasillo@users.noreply.github.com>

--- pages/results_page.py
+++ pages/results_page.py
@@ -346,29 +346,29 @@ def resultPage(job_id):
     result_page = html.Div(final_list, style={'margin': '1%'})
     return result_page
 
-# Generate downlaod link for summary table
 
+# Generate download link summary_by_sample
+@app.callback(
+    [Output('download-link-summary_by_sample', 'children'),
+     Output('interval-summary_by_sample', 'disabled')],
+    [Input('interval-summary_by_sample', 'n_intervals')],
+    [State('div-info-summary_by_sample', 'children'),
+     State('url', 'search')]
+)
+def downloadLinkSample(n, file_to_load, search):  # file to load =
+    if n is None:
+        raise PreventUpdate
+    job_id = search.split('=')[-1]
+    # file_to_load = file_to_load + '.zip'
+    file_to_load = file_to_load + '.txt'
+    file_to_load = file_to_load.strip().split('/')[-1]
+    # print(file_to_load)
+    if os.path.exists(current_working_directory + 'Results/' + job_id + '/' + file_to_load):
+        return html.A('Download file', href=URL+'/Results/' + job_id + '/' + file_to_load, target='_blank'), True
 
-# @app.callback(
-#     [Output('download-link-sumbyposition', 'children'),
-#      Output('interval-sumbyposition', 'disabled')],
-#     [Input('interval-sumbyposition', 'n_intervals')],
-#     [State('div-info-sumbyposition-targets', 'children'),
-#      State('url', 'search')]
-# )
-# def downloadSummaryTable(n, file_to_load, search):
-#     if n is None:
-#         raise PreventUpdate
-#     job_id = search.split('=')[-1]
-#     file_to_load = file_to_load + '.zip'
-#     if os.path.exists(current_working_directory + 'Results/' + job_id + '/' + file_to_load):
-#         return html.A('Download zip', href=URL+'/Results/' + job_id + '/' + file_to_load, target='_blank'), True
-
-#     return 'Generating download link, Please wait...', False
+    return 'Generating download link, Please wait...', False
 
 # Generate download link sumbyposition
-
-
 @app.callback(
     [Output('download-link-sumbyposition', 'children'),
      Output('interval-sumbyposition', 'disabled')],
@@ -406,8 +406,6 @@ def downloadLinkSample(n, file_to_load, search):  # file to load = job_id.HG001.
     return 'Generating download link, Please wait...', False
 
 # Generate download link sumbyguide
-
-
 @app.callback(
     [Output('download-link-sumbyguide', 'children'),
      Output('interval-sumbyguide', 'disabled')],
@@ -964,7 +962,6 @@ def samplePage(job_id, hash):
                         dcc.Interval(interval=5*1000,
                                      id='interval-sumbysample')
                     ]
-
                 )
             ]
         )
@@ -2811,6 +2808,8 @@ def updateContentTab(value, sel_cel, all_guides, search, genome_type):
             html.Div
             (
                 [
+                    html.Div(job_directory + job_id + '.summary_by_samples.' + guide,
+                                        style={'display': 'none'}, id='div-info-summary_by_sample'),
                     dbc.Row(
                         [
                             dbc.Col(html.Div(dcc.Dropdown(
@@ -2824,6 +2823,16 @@ def updateContentTab(value, sel_cel, all_guides, search, genome_type):
                                     'Filter', id='button-filter-population-sample')))
                         ]
                     ),
+                    dbc.Row(
+                        dbc.Col(
+                            html.Div(
+                                [
+                                    html.P('Generating download link, Please wait...', id='download-link-summary_by_sample'),
+                                    dcc.Interval(interval=1*1000, id='interval-summary_by_sample'),
+                                ]
+                            )
+                        )
+                    )
                 ],
                 style={'width': '50%'}
             )
