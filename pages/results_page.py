034fb043332e2c4a48bf71d8007ffd7fa9ea19c4
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-04-30 19:11:35 +0200

    warning linda plot

--- pages/results_page.py
+++ pages/results_page.py
@@ -2630,7 +2630,7 @@ def generate_sample_card(n, sample, sel_cel, all_guides, search):
             f"python {app_main_directory}/PostProcess/CRISPRme_plots_personal.py {integrated_personal} {current_working_directory}/Results/{job_id}/imgs/ {guide}.{sample}.personal > {current_working_directory}/Results/{job_id}/warnings.txt 2>&1")
         os.system(
             f"python {app_main_directory}/PostProcess/CRISPRme_plots_personal.py {integrated_private} {current_working_directory}/Results/{job_id}/imgs/ {guide}.{sample}.private > {current_working_directory}/Results/{job_id}/warnings.txt 2>&1")
-        os.system(f"rm -f {current_working_directory}/Results/{job_id}/warnings.txt {integrated_private} {integrated_personal}")
+        # os.system(f"rm -f {current_working_directory}/Results/{job_id}/warnings.txt {integrated_private} {integrated_personal}")
 
         private = 0
         for line in open(sample_grep_result):
