3fd5b7109ff2f22fb8ebd0861bd56a519fe0593c
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-05-01 19:10:40 +0200

    test hidden P in graph report

--- pages/results_page.py
+++ pages/results_page.py
@@ -3410,8 +3410,7 @@ def updateContentTab(value, sel_cel, all_guides, search, genome_type):
             dbc.Col(
                 html.Div(
                     [
-                        html.Div(
-                        html.P("Select a Superpopulation"),style={samp_style}),
+                        html.P("Select a Superpopulation",style=samp_style),
                         html.Div(
                             dcc.Dropdown(
                                 options=super_populations,
