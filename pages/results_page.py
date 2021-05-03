9d6d7222274dfd0686380bb2e6b6983420e92cf0
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-05-03 18:49:31 +0200

    test result page

--- pages/results_page.py
+++ pages/results_page.py
@@ -3035,12 +3035,20 @@ def updateContentTab(value, sel_cel, all_guides, search, genome_type):
         )
         fl.append(html.Div(
             [
-                dbc.Row(dbc.Col(html.P('Top 100 Personal Targets per CFD score'))),
-                dbc.Row(dbc.Col(html.P('Top 100 Private Targets per CFD score'))),
                 dbc.Row(
                     [
-                        dbc.Col(html.Div('', id='div-personal-plot')),
-                        dbc.Col(html.Div('', id='div-private-plot'))
+                        dbc.Col(
+                            [
+                                html.P('Top 100 Personal Targets per CFD score'),
+                                html.Div('', id='div-personal-plot'),     
+                            ]
+                            ),
+                        dbc.Col(
+                            [
+                                html.P('Top 100 Private Targets per CFD score'),
+                                html.Div('', id='div-private-plot')
+                            ]
+                            )
                     ]
                 )
             ]
