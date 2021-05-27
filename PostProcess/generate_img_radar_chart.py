6ba9cb1cc414c0cadcb391327fb30b4e3268b14c
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-05-27 15:43:20 +0200

    removed subplot adjust in chart

--- PostProcess/generate_img_radar_chart.py
+++ PostProcess/generate_img_radar_chart.py
@@ -219,8 +219,8 @@ def generatePlot(guide, guideDict, motifDict, mismatch, bulge, source):
                  horizontalalignment='center', color='black', size=titlesize)
 
     plt.tight_layout()
-    plt.subplots_adjust(top=0.85, bottom=0.05, left=0.06,
-                        right=0.95, wspace=0.1)
+    # plt.subplots_adjust(top=0.85, bottom=0.05, left=0.06,
+    #                     right=0.95, wspace=0.1)
     plt.savefig(outDir+"/summary_single_guide_" + str(guide) + "_" + str(mismatch) +
                 "."+str(bulge) + '_' + str(source) + "." + file_extension, format=file_extension)
 
