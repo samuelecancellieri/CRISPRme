44f527cdea5dce54f900dd003a1ecfd637847338
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-05-27 18:41:57 +0200

    fix res integrator

--- PostProcess/resultIntegrator.py
+++ PostProcess/resultIntegrator.py
@@ -316,16 +316,22 @@ for nline, line in enumerate(inCrispritzResults):
 
     count_N_in_guide = 0  # check how long is the pam counting Ns in the guide
     pam_at_start = False  # check if pam is at start of the sequence
-    # counr number of Ns in the guide
+    # count number of Ns in the guide
     for count, elem in enumerate(saveDict['real_guide']):
         if elem == 'N':
             count_N_in_guide += 1
             if count == 0:  # if N is at start of the guide, pam_at_start = true
                 pam_at_start = True
-    if pam_at_start:  # save pam sequence extracting directly from the
-        saveDict['pam'] = str(x[3])[:count_N_in_guide]
+    if origin == 'ref':
+        if pam_at_start:  # save pam sequence extracting directly from the ref sequence
+            saveDict['pam'] = str(x[2])[:count_N_in_guide]
+        else:
+            saveDict['pam'] = str(x[2])[-count_N_in_guide:]
     else:
-        saveDict['pam'] = str(x[3])[-count_N_in_guide:]
+        if pam_at_start:  # save pam sequence extracting directly from the var sequence
+            saveDict['pam'] = str(x[3])[:count_N_in_guide]
+        else:
+            saveDict['pam'] = str(x[3])[-count_N_in_guide:]
 
     foundEmpirical = sorted(empiricalTree[int(x[6])-4:int(x[6])+4])
 
