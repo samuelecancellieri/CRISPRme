85c7c90bb3130c1b95ce654cf977cd8bf7285d4e
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-05-31 16:42:39 +0200

    update query table

--- pages/results_page.py
+++ pages/results_page.py
@@ -3310,11 +3310,11 @@ def updateContentTab(value, sel_cel, all_guides, search, genome_type):
         #                   'Bulge Size', 'PAM gen', 'Samples', 'SNP',
         #                   'CFD', 'CFD ref', 'Highest CFD Risk Score',
         #                   'AF', 'Annotation Type']
-        dff = pd.DataFrame(columns=['Bulge_type', 'crRNA', 'DNA', 'Reference', 'Chromosome',
-                                    'Position', 'Direction', 'Mismatches',
-                                    'Bulge_Size', 'PAM_gen', 'Samples', 'SNP',
-                                    'CFD', 'CFD_ref', 'Highest_CFD_Risk_Score',
-                                    'AF', 'Annotation_Type'])  # pd.read_sql_query("SELECT * FROM final_table LIMIT 0",conn) #to define column names in the first empty table
+        dff = pd.DataFrame(columns=['Direction', 'Chromosome', 'Position', 'crRNA', 'DNA',
+                                    'Reference', 'Mismatches', 'Bulge_Size',
+                                    'Total', 'Bulge_type', 'PAM_gen', 'CFD',
+                                    'CFD_ref', 'Highest_CFD_Risk_Score',
+                                    'Var_uniq', 'Annotation_Type'])  # pd.read_sql_query("SELECT * FROM final_table LIMIT 0",conn) #to define column names in the first empty table
         all_value = {'Target1 :with highest CFD': ['Mismatches', 'Bulge_Size', 'Total', 'CFD', 'Highest_CFD_Risk_Score'],  # , 'Highest_CFD_Absolute_Risk_Score'
                      'Target2 :with lowest Mismatches + Bulge Count': ['Mismatches', 'Bulge_Size', 'Total', 'CFD', 'CFD_Risk_Score']}  # , 'CFD_Absolute_Risk_Score'
     # target_options = {'Mismatches': ['Bulge_Size', 'Total', 'CFD'], 'Bulge_Size': ['Mismatches', 'Total', 'CFD'], 'Total': ['Mismatches', 'Bulge_Size', 'CFD'], 'CFD': [
