b448fe8a09cf73cb957069ff4b5ac2eaa74a819c
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-06-01 10:00:59 +0200

    test sql query to read header

--- pages/results_page.py
+++ pages/results_page.py
@@ -3314,8 +3314,10 @@ def updateContentTab(value, sel_cel, all_guides, search, genome_type):
                                     'Reference', 'Mismatches', 'Bulge_Size',
                                     'Total', 'Bulge_type', 'PAM_gen', 'CFD',
                                     'CFD_ref', 'Highest_CFD_Risk_Score',
-                                    'Var_uniq', 'Annotation_Type'])  # pd.read_sql_query("SELECT * FROM final_table LIMIT 0",conn) #to define column names in the first empty table
-        print('check col in query table', dff)
+                                    'Var_uniq', 'Annotation_Type'])
+        # to define column names in the first empty table
+        print(pd.read_sql_query("SELECT * FROM final_table LIMIT 0", conn))
+        # print('check col in query table', dff)
         all_value = {'Target1 :with highest CFD': ['Mismatches', 'Bulge_Size', 'Total', 'CFD', 'Highest_CFD_Risk_Score'],  # , 'Highest_CFD_Absolute_Risk_Score'
                      'Target2 :with lowest Mismatches + Bulge Count': ['Mismatches', 'Bulge_Size', 'Total', 'CFD', 'CFD_Risk_Score']}  # , 'CFD_Absolute_Risk_Score'
     # target_options = {'Mismatches': ['Bulge_Size', 'Total', 'CFD'], 'Bulge_Size': ['Mismatches', 'Total', 'CFD'], 'Total': ['Mismatches', 'Bulge_Size', 'CFD'], 'CFD': [
