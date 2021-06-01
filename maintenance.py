13604ad57a28a2fea6d42438295fd7f6d2d02b02
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-06-01 17:44:14 +0200

    maintenance page

--- maintenance.py
+++ maintenance.py
@@ -7,17 +7,19 @@ import flask
 import pandas as pd
 import time
 import os
+from pages import navbar_creation
 
 server = flask.Flask('app')
 
 app = dash.Dash('app', server=server)
 
-app.scripts.config.serve_locally = False
-dcc._js_dist[0]['external_url'] = 'https://cdn.plot.ly/plotly-basic-latest.min.js'
-
+navbar = navbar_creation.Navbar()
 app.layout = html.Div(
-    'We are sorry, CRISPRme website is under maintenance, we will be back online as soon as possible.\nThanks for yout patience.')
-
+    [
+        navbar,
+        html.P('We are sorry, CRISPRme website is under maintenance, we will be back online as soon as possible.\nThanks for yout patience.')
+    ]
+)
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', port=80, debug=False,
                    dev_tools_ui=False, dev_tools_props_check=False)
