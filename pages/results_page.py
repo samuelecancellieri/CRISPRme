77799d001d572dc4f0782c73296f320f75dc5ac3
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-06-01 15:54:18 +0200

    test header names for query table

--- pages/results_page.py
+++ pages/results_page.py
@@ -3988,6 +3988,7 @@ def update_output(n_clicks, page_current, page_size, sel_cel, target, radio_orde
     else:
         raise PreventUpdate
     # #print('query table', data)
+    data.columns = COL_BOTH
     return data, tooltip_data, alert
 
 
