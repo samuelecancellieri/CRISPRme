d486acdc19c33aad0d6b505395796e067579bf5d
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-06-03 01:27:01 +0200

    test set height for table cells

--- PostProcess/generate_img_radar_chart.py
+++ PostProcess/generate_img_radar_chart.py
@@ -179,6 +179,8 @@ def generatePlot(guide, guideDict, motifDict, mismatch, bulge, source):
     # table.scale(1, 2)
     cellDict = table.get_celld()
     print(cellDict)
+    for count in range(11):
+        cellDict[(count, -1)].set_height(0.15)
     for i in range(1, -1, -1):
         cellDict[(0, i)].set_height(0.1)
         for j in range(0, len(categories_names)+1):
