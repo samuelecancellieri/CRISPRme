f4ae4e845fa097df37d240f3988fdf12be5a40c1
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-06-03 01:43:06 +0200

    table categorires

--- PostProcess/generate_img_radar_chart.py
+++ PostProcess/generate_img_radar_chart.py
@@ -172,7 +172,7 @@ def generatePlot(guide, guideDict, motifDict, mismatch, bulge, source):
     # print('transpose_list', transpose_list)
 
     plt.axis('off')
-    table = plt.table(cellText=transpose_list, rowLabels=categories_names, colLabels=['Total', 'Percentage'],
+    table = plt.table(cellText=transpose_list, rowLabels=categories, colLabels=['Total', 'Percentage'],
                       loc='best', colWidths=[0.25, 0.35])
     table.auto_set_font_size(False)
     table.set_fontsize(fontsize)
