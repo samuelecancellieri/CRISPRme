3b94575979b5a24ad3877d1644d8be605356e8fa
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-06-02 16:03:57 +0200

    update annot personal

--- pages/main_page.py
+++ pages/main_page.py
@@ -227,10 +227,10 @@ def changeUrl(n, href, nuclease, genome_selected, ref_var, annotation_var, vcf_i
                 f"cp {current_working_directory}/Annotations/gencode_encode.hg38.bed {current_working_directory}/Annotations/ann_tmp_{job_id}.bed")
             os.system(
                 f'awk \'$4 = $4\"_personal\"\' {current_working_directory}/Annotations/{annotation_input} | sed "s/ /\t/g" > {current_working_directory}/Annotations/{annotation_input}.tmp')
+            # os.system(
+            #     f'mv {current_working_directory}/Annotations/{annotation_input}.tmp {current_working_directory}/Annotations/{annotation_input}')
             os.system(
-                f'mv {current_working_directory}/Annotations/{annotation_input}.tmp {current_working_directory}/Annotations/{annotation_input}')
-            os.system(
-                f"tail -n +1 {current_working_directory}/Annotations/{annotation_input} >> {current_working_directory}/Annotations/ann_tmp_{job_id}.bed")
+                f"tail -n +1 {current_working_directory}/Annotations/{annotation_input}.tmp >> {current_working_directory}/Annotations/ann_tmp_{job_id}.bed")
             os.system(
                 f"mv {current_working_directory}/Annotations/ann_tmp_{job_id}.bed {current_working_directory}/Annotations/{annotation_name}")
     elif 'MA' in annotation_var:
