bc48b882e2d49e2838f96fef593d963bfc47c0d6
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-06-02 22:31:55 +0200

    update result integrator

--- PostProcess/resultIntegrator.py
+++ PostProcess/resultIntegrator.py
@@ -315,11 +315,8 @@ for nline, line in enumerate(inCrispritzResults):
                     firstcomp += rev_comp(piece)
                 for piece in str(split[3]):
                     secondcomp += rev_comp(piece)
-                try:
-                    variantList[count] = ''.join(
-                        reversed(firstcomp))+str(int(var_pos[count])+correction)+''.join(reversed(secondcomp))
-                except:
-                    pass
+                variantList[count] = ''.join(
+                    reversed(firstcomp))+str(int(var_pos[count])+correction)+''.join(reversed(secondcomp))
     variantList_highest_cfd = variantList
 
     variantList = ['NA']
@@ -361,11 +358,8 @@ for nline, line in enumerate(inCrispritzResults):
                     firstcomp += rev_comp(piece)
                 for piece in str(split[3]):
                     secondcomp += rev_comp(piece)
-                try:
-                    variantList[count] = ''.join(
-                        reversed(firstcomp))+str(int(var_pos[count])+correction)+''.join(reversed(secondcomp))
-                except:
-                    pass
+                variantList[count] = ''.join(
+                    reversed(firstcomp))+str(int(var_pos[count])+correction)+''.join(reversed(secondcomp))
     variantList_fewest_mm_b = variantList
 
     saveDict['Highest_CFD_Strand'] = str(x[7])
