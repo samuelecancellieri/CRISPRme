8cfd987019691de322e442428a0b3f0f65cff527
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-06-03 01:21:32 +0200

    print celldict

--- PostProcess/generate_img_radar_chart.py
+++ PostProcess/generate_img_radar_chart.py
@@ -178,10 +178,11 @@ def generatePlot(guide, guideDict, motifDict, mismatch, bulge, source):
     table.set_fontsize(fontsize)
     # table.scale(1, 2)
     cellDict = table.get_celld()
-    for i in range(3):
-        cellDict[(0, i)].set_height(0.1)
-        for j in range(0, len(categories_names)+1):
-            cellDict[(j, i)].set_height(0.15)
+    print(cellDict)
+    # for i in range(3):
+    #     cellDict[(0, i)].set_height(0.1)
+    #     for j in range(0, len(categories_names)+1):
+    #         cellDict[(j, i)].set_height(0.15)
 
     totalMotif = [0]*len(guide)
     for count in range(len(guide)):
