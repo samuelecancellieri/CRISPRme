83b44837ed581bc66ad9af4750f3194bb1d586fc
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-06-03 01:40:23 +0200

    cell dimensions

--- PostProcess/generate_img_radar_chart.py
+++ PostProcess/generate_img_radar_chart.py
@@ -172,19 +172,19 @@ def generatePlot(guide, guideDict, motifDict, mismatch, bulge, source):
     # print('transpose_list', transpose_list)
 
     plt.axis('off')
-    table = plt.table(cellText=transpose_list, rowLabels=categories, colLabels=['Total', 'Percentage'],
+    table = plt.table(cellText=transpose_list, rowLabels=categories_names, colLabels=['Total', 'Percentage'],
                       loc='best', colWidths=[0.25, 0.35])
     table.auto_set_font_size(False)
     table.set_fontsize(fontsize)
-    table.scale(1, 1.1)
-    # cellDict = table.get_celld()
+    # table.scale(1, 1.1)
+    cellDict = table.get_celld()
     # # print(cellDict)
-    # for count in range(1, 11, 1):
-    #     cellDict[(count, -1)].set_height(0.2)
-    # for i in range(1, -1, -1):
-    #     cellDict[(0, i)].set_height(0.1)
-    #     for j in range(0, len(categories_names)+1):
-    #         cellDict[(j, i)].set_height(0.2)
+    for count in range(1, 11, 1):
+        cellDict[(count, -1)].set_height(0.2)
+    for i in range(1, -1, -1):
+        cellDict[(0, i)].set_height(0.1)
+        for j in range(0, len(categories_names)+1):
+            cellDict[(j, i)].set_height(0.2)
 
     totalMotif = [0]*len(guide)
     for count in range(len(guide)):
@@ -243,7 +243,7 @@ def generatePlot(guide, guideDict, motifDict, mismatch, bulge, source):
                  horizontalalignment='center', color='black', size=titlesize)
 
     plt.tight_layout()
-    plt.subplots_adjust(wspace=0.1)
+    # plt.subplots_adjust(wspace=0.1)
     plt.savefig(outDir+"/summary_single_guide_" + str(guide) + "_" + str(mismatch) +
                 "."+str(bulge) + '_' + str(source) + "." + file_extension, format=file_extension)
 
