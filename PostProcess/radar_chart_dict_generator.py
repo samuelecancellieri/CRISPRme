d3e1d026fbc09d50f8b9d09f23f297646d122fec
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-06-07 11:53:27 +0200

    remove print in radar chart gene

--- PostProcess/radar_chart_dict_generator.py
+++ PostProcess/radar_chart_dict_generator.py
@@ -491,7 +491,7 @@ for line in inAnnotationsFile:
 
 annotationsSet.add('CTCF-only')
 annotationsSet = sorted(annotationsSet)
-print('annotations', annotationsSet)
+# print('annotations', annotationsSet)
 
 # read all the population, superpop and samples
 for line in inSamplesIDFile:
