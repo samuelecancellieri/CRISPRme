a4d6726c8fca9aa7313778d70ccad6f75dc19c01
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-06-16 10:46:55 +0200

    test no conversion on sample for df extraction

--- pages/results_page.py
+++ pages/results_page.py
@@ -3178,7 +3178,8 @@ def generate_sample_card(n, sample, sel_cel, all_guides, search):
     job_directory = current_working_directory + 'Results/' + job_id + '/'
     file_to_grep = job_directory + '.' + job_id + '.bestMerge.txt'
     sample_grep_result = current_working_directory + 'Results/' + \
-        job_id + '/' + job_id + '.' + sample + '.' + guide + '.private.txt'
+        job_id + '/' + job_id + '.' + \
+        str(sample) + '.' + guide + '.private.txt'
     if not os.path.exists(current_working_directory + 'Results/' + job_id + '/' + job_id + '.' + sample + '.' + guide + '.sample_card.txt'):
         df = pd.read_csv(job_directory + job_id + '.summary_by_samples.' +
                          guide+'.txt', sep='\t', skiprows=2, index_col=0, header=None, na_filter=False, dtype=object)
