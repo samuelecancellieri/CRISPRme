c039667d0a98cbe790d0f610c64a6227128c7211
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-06-30 17:14:02 +0200

    typo in complete test script

--- crisprme_test_complete_package.sh
+++ crisprme_test_complete_package.sh
@@ -24,6 +24,7 @@ if [ "$md5_VCF_package" != "$test_md5" ]; then
     exit 1
 fi
 
+echo "unziping all packages"
 mkdir crisprme_complete_test
 tar -xzf complete_test_package_no_VCFs.tar.gz --directory crisprme_complete_test/
 tar -xzf VCFs.tar.gz --directory crisprme_complete_test/
@@ -35,4 +36,4 @@ echo "hg38_1000G/" >list_vcf.txt
 echo "hg38_1000G.samplesID.txt" >list_samplesID.txt
 echo "crisprme.py complete-search --genome Genomes/hg38/ --vcf list_vcf.txt/ --guide sg1617.txt --pam PAMs/20bp-NNN-SpCas9.txt --annotation Annotations/encode+gencode.hg38.bed --samplesID list_samplesID.txt --gene_annotation Annotations/gencode.protein_coding.bed --bMax 2 --mm 6 --bDNA 2 --bRNA 2 --merge 3 --output sg1617.6.2.2 --thread 8" >crisprme_complete_auto_test.sh
 
-echo "to test the software, launch  crisprme_complete_auto_test.sh"
+echo "to test the software, launch crisprme_complete_auto_test.sh"
