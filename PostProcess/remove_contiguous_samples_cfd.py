c89818410147bea1149763317a6f9790bd7cdcb9
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-08-31 14:56:04 +0200

    update to solve sorting with non-scorable targets

--- PostProcess/remove_contiguous_samples_cfd.py
+++ PostProcess/remove_contiguous_samples_cfd.py
@@ -47,7 +47,9 @@ def get_best_targets(cluster, fileOut, fileOut_disc, cfd, snp_info):
             fileOut.write("\t".join(best_var))
         else:
             fileOut.write("\t".join(best_var))
-'''   
+'''
+
+
 def get_best_targets(cluster, fileOut, fileOut_disc, cfd, snp_info):
     final_list = []
     list_ref = []
@@ -56,14 +58,18 @@ def get_best_targets(cluster, fileOut, fileOut_disc, cfd, snp_info):
         if ele[snp_info] == 'n':
             list_ref.append(ele)
         else:
-            if (ele[pos], ele[snp_info]) in dict_var.keys(): #merge samples of identical targets (coming from different VCF datasets)
-                dict_var[(ele[pos], ele[snp_info])][0][true_guide - 2] = dict_var[(ele[pos],ele[snp_info])][0][true_guide - 2] + "," + ele[true_guide - 2] #true_guide - 2 points to samples column
-                dict_var[(ele[pos], ele[snp_info])][0][snp_info - 2] = dict_var[(ele[pos],ele[snp_info])][0][snp_info - 2] + "," + ele[snp_info - 2] #snp_info - 2 points to rsID column
-                dict_var[(ele[pos], ele[snp_info])][0][snp_info - 1] = dict_var[(ele[pos],ele[snp_info])][0][snp_info - 1] + "," + ele[snp_info - 1] #ttuesnp_info_guide - 2 points to AF column
+            # merge samples of identical targets (coming from different VCF datasets)
+            if (ele[pos], ele[snp_info]) in dict_var.keys():
+                dict_var[(ele[pos], ele[snp_info])][0][true_guide - 2] = dict_var[(ele[pos], ele[snp_info])
+                                                                                  ][0][true_guide - 2] + "," + ele[true_guide - 2]  # true_guide - 2 points to samples column
+                dict_var[(ele[pos], ele[snp_info])][0][snp_info - 2] = dict_var[(ele[pos], ele[snp_info])
+                                                                                ][0][snp_info - 2] + "," + ele[snp_info - 2]  # snp_info - 2 points to rsID column
+                dict_var[(ele[pos], ele[snp_info])][0][snp_info - 1] = dict_var[(ele[pos], ele[snp_info])
+                                                                                ][0][snp_info - 1] + "," + ele[snp_info - 1]  # ttuesnp_info_guide - 2 points to AF column
             else:
                 dict_var[(ele[pos], ele[snp_info])] = [ele]
-    
-    list_ref.sort(key = lambda x : int(x[total]))
+
+    list_ref.sort(key=lambda x: int(x[total]))
     var_only = False
     if len(list_ref) > 1:
         best_ref = list_ref[0]
@@ -75,10 +81,10 @@ def get_best_targets(cluster, fileOut, fileOut_disc, cfd, snp_info):
         final_list.append(list_ref[0])
     else:
         var_only = True
-    
+
     for key in dict_var.keys():
         list_var = dict_var[key]
-        list_var.sort(key = lambda x : int(x[total]))
+        list_var.sort(key=lambda x: int(x[total]))
         best_var = list_var[0]
         if len(list_var) > 1:
             for ele_var in list_var[1:]:
@@ -91,16 +97,23 @@ def get_best_targets(cluster, fileOut, fileOut_disc, cfd, snp_info):
             if var_only:
                 best_var[12] = 'y'
             final_list.append(best_var)
-    
+
     n_ele = len(final_list)
     if n_ele > 1:
-        final_list.sort(key = lambda x : float(x[cfd]), reverse=True)
-        if final_list[0][cfd] == final_list[1][cfd] and final_list[1][cfd-2] == 'n':
-            final_list[1][cfd-1] = str(n_ele-1)
-            final_list[1][2*cfd+1] = str(n_ele-1)
-            fileOut.write("\t".join(final_list[1]))
-            bestTarget = final_list.pop(1)
+        if str(final_list[0][cfd]) != '-1.0':
+            final_list.sort(key=lambda x: float(x[cfd]), reverse=True)
+            if final_list[0][cfd] == final_list[1][cfd] and final_list[1][cfd-2] == 'n':
+                final_list[1][cfd-1] = str(n_ele-1)
+                final_list[1][2*cfd+1] = str(n_ele-1)
+                fileOut.write("\t".join(final_list[1]))
+                bestTarget = final_list.pop(1)
+            else:
+                final_list[0][cfd-1] = str(n_ele-1)
+                final_list[0][2*cfd+1] = str(n_ele-1)
+                fileOut.write("\t".join(final_list[0]))
+                bestTarget = final_list.pop(0)
         else:
+            final_list.sort(key=lambda x: (int(x[total-1], int(x[total-2]))))
             final_list[0][cfd-1] = str(n_ele-1)
             final_list[0][2*cfd+1] = str(n_ele-1)
             fileOut.write("\t".join(final_list[0]))
@@ -108,17 +121,18 @@ def get_best_targets(cluster, fileOut, fileOut_disc, cfd, snp_info):
         for ele in final_list:
             ele[cfd-1] = str(n_ele-1)
             ele[2*cfd+1] = str(n_ele-1)
-            #fileOut_disc.write(("\t".join(ele)).strip()+"\t"+bestTarget[chrom]+"_"+bestTarget[pos]+"\n")
+            # fileOut_disc.write(("\t".join(ele)).strip()+"\t"+bestTarget[chrom]+"_"+bestTarget[pos]+"\n")
             fileOut_disc.write(("\t".join(ele)))
     elif n_ele == 1:
         fileOut.write("\t".join(final_list[0]))
 
+
 tau = int(sys.argv[3])
-chrom = int(sys.argv[4])-1 
-pos = int(sys.argv[5])-1 
+chrom = int(sys.argv[4])-1
+pos = int(sys.argv[5])-1
 total = int(sys.argv[6])-1
 true_guide = int(sys.argv[7])-1
-snp_info = int(sys.argv[8])-1 
+snp_info = int(sys.argv[8])-1
 cfd = int(sys.argv[9])-1
 # -1 is to get the correct "python enumeration" from the bash script
 
@@ -138,7 +152,8 @@ with open(sys.argv[1], 'r') as fileIn:
             for line in fileIn:
                 splitted = line.split("\t")
                 if prev_guide != splitted[true_guide] or prev_chr != splitted[chrom] or int(splitted[pos]) - prev_pos > tau:
-                    get_best_targets(cluster, fileOut, fileOut_disc, cfd, snp_info)
+                    get_best_targets(cluster, fileOut,
+                                     fileOut_disc, cfd, snp_info)
                     cluster = [splitted]
                 else:
                     cluster.append(splitted)
@@ -146,7 +161,7 @@ with open(sys.argv[1], 'r') as fileIn:
                 prev_pos = int(splitted[pos])
                 prev_chr = splitted[chrom]
                 prev_snp = splitted[snp_info]
-            
+
             get_best_targets(cluster, fileOut, fileOut_disc, cfd, snp_info)
 
 print("Mergin done in: "+str(time.time()-start))
