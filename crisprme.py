d7c728ab2b4813f20f85344d876a1ab99c408a7a
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-02-23 16:26:09 +0100

    update to activate default local mode for web interface

--- crisprme.py
+++ crisprme.py
@@ -838,7 +838,7 @@ def web_interface():
         print("This function must be launched without input, it starts a local server to use the web interface.")
         print("Open your web-browser and write 127.0.0.1:8080 in the search bar if you are executing locally, if you are executing on an external server write <yourserverip>:8080 in search bar")
         exit(0)
-    subprocess.run(corrected_web_path+'/./index.py', '--local')
+    subprocess.run(corrected_web_path+'/./index.py')
 
 
 # HELP FUNCTION
