d88f32945d1c78926cc1f1b396a50b93a7539002
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-02-23 14:23:08 +0100

    remove targets after execution

--- PostProcess/submit_job_automated_new_multiple_vcfs.sh
+++ PostProcess/submit_job_automated_new_multiple_vcfs.sh
@@ -612,8 +612,7 @@ echo -e "Cleaning directory"
 rm -f *.CFDGraph.txt
 rm -f indels.CFDGraph.txt
 rm -r "crispritz_prof"
-#keep targets to check them
-# rm -r "crispritz_targets" #remove targets in online version to avoid memory saturation
+rm -r "crispritz_targets" #remove targets in online version to avoid memory saturation
 #prevent deletion of single best file to use in further creations of data files (summary,charts and plots)
 # rm $final_res.bestCFD.txt
 # rm $final_res.bestmmblg.txt
