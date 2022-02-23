daf4fe8a6426059cd6f4bb08c948d2b4dc0c831d
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-02-23 22:55:18 +0100

    test private table

--- pages/results_page.py
+++ pages/results_page.py
@@ -3703,8 +3703,10 @@ def generate_sample_card(n, filter_criterion, sample, sel_cel, all_guides, searc
         [query_cols['sort']], ascending=[order])
     # extract sample private targets
     result_private = result_personal[result_personal[query_cols['samples']] == sample]
+    print('result prima', result_private)
     drop_cols = drop_columns(result_private, filter_criterion)
     result_private.drop(drop_cols, axis=1, inplace=True)
+    print('result dopo', result_private)
 
     # print(result_personal)
     # print(result_private)
